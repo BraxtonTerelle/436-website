bb6a31e4c6f696ad9e2ff7ee067dddd66cf59830
jakegridley <jakegridley@arizona.edu>
2023-10-20 15:27:29 -0700

    input from user

--- frontend/src/pages/Services.js
+++ frontend/src/pages/Services.js
@@ -1,8 +1,12 @@
 function Services() {
   return (
+    <>
     <div>
       <p>This is the services page</p>
     </div>
+    <input type="date"></input>
+    <button>Submit</button>
+    </>
   );
 }
 
