068e00a6c2985277b1e1436f2ab57a4de5f058b9
Braxton Little <braxtonlittle@arizona.edu>
2023-10-19 21:53:28 -0700

    React Router now handles navigation between pages

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,30 +1,9 @@
-
-
-
-function AboutPage() {
-    return (
-        <>
-            <div id="titleDiv">
-                <h1 id="header">About</h1>
-            </div>
-            
-            <div id="contentDiv">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id ex sit amet leo hendrerit 
-                molestie. Duis aliquet condimentum tellus. Mauris a purus tincidunt, blandit justo 
-                malesuada, placerat nibh. Sed in orci placerat, tincidunt nibh ut, fringilla neque. 
-                Praesent blandit velit eu iaculis efficitur. Nullam ut blandit leo. Quisque pellentesque 
-                elit tortor, id ornare arcu finibus id. Nulla at ex sed justo tempus venenatis ac eu massa. 
-                In malesuada felis sit amet magna tempor, in maximus leo ultricies. Morbi in volutpat urna. 
-                Maecenas sed orci sed lectus efficitur elementum.
-                </p>
-            </div>
-
-            <div id="contactDiv">
-
-            </div>
-
-        </>
-    )
+function About() {
+  return (
+    <div>
+      <p>This is the about page</p>
+    </div>
+  );
 }
 
+export default About;
