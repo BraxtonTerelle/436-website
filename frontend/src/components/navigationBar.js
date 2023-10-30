ba3b1f839ed68585b5dd8dbe510b67a6fb3f4f26
Braxton Little <braxtonlittle@arizona.edu>
2023-10-30 10:44:44 -0700

    Changed styling for homepage and navbar functionality

--- frontend/src/components/navigationBar.js
+++ frontend/src/components/navigationBar.js
@@ -1,80 +1,55 @@
 import "../styles/NavigationBar.css";
-import { Link } from "react-router-dom";
-// Helper function that deselects a menu button specified by
-// its button id
-function deselectNavButton(buttonId) {
-  var button = document.getElementById(buttonId);
-  button.className = "navButton";
-}
-
-// Helper function that selects a menu button specified by its
-// button id
-function selectNavButton(buttonId) {
-  var button = document.getElementById(buttonId);
-  button.className = "selectedNavButton";
-}
-
-// Based on which button was pressed, select that menu nav
-// button and deselect all others since they may have been
-// previously pressed
-function updateNavButtons(buttonName) {
-  switch (buttonName) {
-    case "Home":
-      selectNavButton("homeButton");
-      deselectNavButton("servicesButton");
-      deselectNavButton("aboutButton");
-      break;
-    case "Services":
-      selectNavButton("servicesButton");
-      deselectNavButton("homeButton");
-      deselectNavButton("aboutButton");
-      break;
-    case "About":
-      selectNavButton("aboutButton");
-      deselectNavButton("servicesButton");
-      deselectNavButton("homeButton");
-      break;
-  }
-}
+import { useNavigate } from "react-router-dom";
 
 // This component returns a an absolutely positioned div
 // that represents the navigation bar for the website
 function NavigationBar(props) {
+  const url = window.location.href;
+  var currentPage;
+  if (url.includes("/")) {
+    const path = url.substring(url.lastIndexOf("/"));
+    if (path.includes("services")) {
+      currentPage = "Services";
+    } else if (path.includes("about")) {
+      currentPage = "About";
+    } else {
+      currentPage = "Home";
+    }
+  } else {
+    currentPage = "Home";
+  }
+  const navigate = useNavigate();
   return (
     <div className="navContainer">
-      <Link to="/">
-        <button
-          id="homeButton"
-          className="selectedNavButton"
-          onClick={() => {
-            updateNavButtons("Home");
-          }}
-        >
-          Home
-        </button>
-      </Link>
-      <Link to="/services">
-        <button
-          id="servicesButton"
-          className="navButton"
-          onClick={() => {
-            updateNavButtons("Services");
-          }}
-        >
-          Services
-        </button>
-      </Link>
-      <Link to="/about">
-        <button
-          id="aboutButton"
-          className="navButton"
-          onClick={() => {
-            updateNavButtons("About");
-          }}
-        >
-          About
-        </button>
-      </Link>
+      <button
+        id="homeButton"
+        className={currentPage == "Home" ? "selectedNavButton" : "navButton"}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Home
+      </button>
+      <button
+        id="servicesButton"
+        className={
+          currentPage == "Services" ? "selectedNavButton" : "navButton"
+        }
+        onClick={() => {
+          navigate("/services");
+        }}
+      >
+        Services
+      </button>
+      <button
+        id="aboutButton"
+        className={currentPage == "About" ? "selectedNavButton" : "navButton"}
+        onClick={() => {
+          navigate("/about");
+        }}
+      >
+        About
+      </button>
     </div>
   );
 }
