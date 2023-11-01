ce4c3785688665f8624e5616becb17c9696878f1
Braxton Little <braxtonlittle@arizona.edu>
2023-11-01 12:26:47 -0700

    Dummy appointments before presentation

--- frontend/src/pages/Appointments.js
+++ frontend/src/pages/Appointments.js
@@ -4,36 +4,53 @@ import "../styles/Appointments.css";
 function getAppointmentsRange() {
   var startDate = document.getElementById("startDate").value;
   var endDate = document.getElementById("endDate").value;
-  var available = document.getElementById("availableBox").value;
 
   //var url = 'https://jsonplaceholder.typicode.com/posts';
 
-  var url = "http://localhost:8080/getAppointments?date=%2210/20/2023";
+  /*var url = "http://localhost:8080/getAppointments?date=%2210/20/2023";
 
   console.log(startDate);
   console.log(endDate);
-  console.log(available);
 
   fetch(url)
     .then((res) => {
       return res.text();
     })
     .then((text) => {
-      document.getElementById("res1Div").innerText = text;
+      document.getElementById("res1Div").innerText = JSON.stringify({
+        date: { month: 10, day: 25, year: 2023 },
+        time: { hour: 11, minute: 15 },
+        duration: { hour: 2, minute: 0 },
+        contactInfo: {
+          firstName: "Kharizia",
+          lastName: "Ramos",
+          email: "kharizia@arizona.edu",
+          phoneNumber: "111-222-3333",
+        },
+      });
     })
     .catch((err) => {
       console.error(err);
-    });
+    });*/
+  document.getElementById("res1Div").innerText = JSON.stringify({
+    date: { month: 10, day: 25, year: 2023 },
+    time: { hour: 11, minute: 15 },
+    duration: { hour: 2, minute: 0 },
+    contactInfo: {
+      firstName: "Kharizia",
+      lastName: "Ramos",
+      email: "kharizia@arizona.edu",
+      phoneNumber: "111-222-3333",
+    },
+  });
 }
 
 function getAppointmentsDate() {
   var date = document.getElementById("date").value;
-  var available = document.getElementById("availableBox").value;
 
   console.log(date);
-  console.log(available);
 
-  var url = `http://localhost:8080/getAppointments?date=${date}`;
+  /*var url = `http://localhost:8080/getAppointments?date=${date}`;
 
   fetch(url)
     .then((res) => {
@@ -44,12 +61,68 @@ function getAppointmentsDate() {
     })
     .catch((err) => {
       console.error(err);
+    });*/
+  document.getElementById("res2Div").innerText = JSON.stringify({
+    date: { month: 10, day: 22, year: 2023 },
+    time: { hour: 10, minute: 45 },
+    duration: { hour: 1, minute: 30 },
+    contactInfo: {
+      firstName: "Jake",
+      lastName: "Gridley",
+      email: "jake@arizona.edu",
+      phoneNumber: "111-222-3333",
+    },
+  });
+}
+
+function createAppointment() {
+  var url = `http://localhost:8080/createAppointment`;
+  var body = {
+    date: {
+      month: 10,
+      day: 20,
+      year: 2023,
+    },
+    time: {
+      hour: 13,
+      minute: 30,
+    },
+    duration: {
+      hours: 2,
+      minutes: 0,
+    },
+    contactInfo: {
+      firstName: "Braxton",
+      lastName: "Little",
+      email: "braxton@arizona.edu",
+      phoneNumber: "111-222-3333",
+    },
+  };
+
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((text) => {
+      text.json().then((result) => {
+        document.getElementById("res3Div").innerText = JSON.stringify(result);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
     });
 }
 
 function AppointmentsPage() {
   return (
     <>
+      <div className="titleContainer">
+        <h1 className="titleHeader">Hair by Kharizia</h1>
+      </div>
       <div id="aptsPage">
         <div class="aptsBox" id="viewRangeDiv">
           <div id="input1Div">
@@ -66,11 +139,6 @@ function AppointmentsPage() {
           <div id="res1Div"></div>
         </div>
 
-        <div id="checkBoxDiv">
-          <input type="checkbox" id="availableBox"></input>
-          <label>Check To See Available Appointments</label>
-        </div>
-
         <div class="aptsBox" id="viewDateDiv">
           <div id="input2Div">
             View Appointments On:
@@ -84,9 +152,24 @@ function AppointmentsPage() {
 
           <div id="res2Div"></div>
         </div>
+
+        <div class="aptsBox" id="viewRangeDiv">
+          <div id="input3Div">
+            Create Appointment:
+            <br></br>
+            <input type="date" id="startDate"></input>
+            <input type="date" id="endDate"></input>
+            <br></br>
+            <button id="searchApts" onClick={createAppointment}>
+              Create
+            </button>
+          </div>
+
+          <div id="res3Div"></div>
+        </div>
       </div>
 
-      <Footer color="primary" />
+      <Footer color="secondary" />
     </>
   );
 }
